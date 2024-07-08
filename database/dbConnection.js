82697874ecc1758d6ac101f5d3996efba58287fe
Dhairya <pmdhairya56@gmail.com>
2024-07-08 23:25:55 +0530

    first commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection =() => {
     mongoose.connect(process.env.MONGO_URI,{
-        dbName: "HMS_DPD",
+        dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
     }).then(()=>{
         console.log("Database connected successfully");
     })  
